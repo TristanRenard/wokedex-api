c10045d663b88421c7173894c73b7ac33b5329d8
TristanRenard <renard.tristan@proton.me>
2025-07-22 12:04:44 +0200

    fix: pre-commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,30 +1,30 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat',     // Nouvelle fonctionnalité
-        'fix',      // Correction de bug
-        'docs',     // Documentation
-        'style',    // Formatage, point-virgules manquants, etc.
-        'refactor', // Refactoring de code
-        'perf',     // Amélioration de performance
-        'test',     // Ajout ou modification de tests
-        'chore',    // Tâches de maintenance
-        'ci',       // Configuration CI/CD
-        'build',    // Build system ou dépendances externes
-        'revert'    // Revert d'un commit précédent
-      ]
+        "feat", // Nouvelle fonctionnalité
+        "fix", // Correction de bug
+        "docs", // Documentation
+        "style", // Formatage, point-virgules manquants, etc.
+        "refactor", // Refactoring de code
+        "perf", // Amélioration de performance
+        "test", // Ajout ou modification de tests
+        "chore", // Tâches de maintenance
+        "ci", // Configuration CI/CD
+        "build", // Build system ou dépendances externes
+        "revert", // Revert d'un commit précédent
+      ],
     ],
-    'type-case': [2, 'always', 'lower'],
-    'type-empty': [2, 'never'],
-    'subject-case': [2, 'always', 'lower'],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [2, 'always']
-  }
-} 
+    "type-case": [2, "always", "lowerCase"],
+    "type-empty": [2, "never"],
+    "subject-case": [2, "always", "lowerCase"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+  },
+}
