edf47dd1bd3a7532385186662a30f56693b68ff0
TristanRenard <renard.tristan@proton.me>
2025-10-10 14:59:15 +0200

    fix: correct response structure for user data in @me endpoint

--- src/index.ts
+++ src/index.ts
@@ -110,7 +110,7 @@ app.route("/tags", tags)
 app.route("/cards", cards)
 
 app.get("/@me", optionalAuthMiddleware, (c: OptionalAuthContext) =>
-  c.json({ succes: Boolean(c?.user?.username), username: c?.user?.username }),
+  c.json({ succes: Boolean(c?.user?.username), user: c?.user }),
 )
 
 // Serveur HTTP
