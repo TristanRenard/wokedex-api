38a7354646f0626a990ffc02cdcf2b245462249d
TristanRenard <renard.tristan@proton.me>
2025-10-13 13:21:14 +0200

    fix(umami): refactor tracking to force hostname

--- src/umami.ts
+++ src/umami.ts
@@ -3,12 +3,36 @@ import { Umami } from "@umami/node"
 
 const isUmamiConfigured =
   process.env.UMAMI_WEBSITE_ID && process.env.UMAMI_HOSTURL
+
+class UmamiWithHostname extends Umami {
+  private systemHostname: string
+
+  constructor(config: { websiteId: string; hostUrl: string }) {
+    super(config)
+    this.systemHostname = process.env.HOST_NAME as string
+  }
+
+  // eslint-disable-next-line no-restricted-syntax
+  async track(
+    eventName: string,
+    data?: Record<string, string | number | Date>,
+  ): Promise<Response> {
+    return super.track(eventName, {
+      ...data,
+      hostname: this.systemHostname,
+    })
+  }
+}
+
 const umami = isUmamiConfigured
-  ? new Umami({
-      websiteId: process.env.UMAMI_WEBSITE_ID,
-      hostUrl: process.env.UMAMI_HOSTURL,
+  ? new UmamiWithHostname({
+      websiteId: process.env.UMAMI_WEBSITE_ID as string,
+      hostUrl: process.env.UMAMI_HOSTURL as string,
     })
   : null
+
+console.log(umami)
+
 const track = async (
   eventName: string,
   data?: Record<string, string | number | Date>,
